c15d8cf94d83ed750b834a1b9d2dd4ce30665f72
vsbron <vsbron@gmail.com>
2024-08-14 14:22:25 +0300

    Finished Services section

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -21,6 +21,7 @@ function Services() {
           text="Brainwave unlocks the potential of AI-powered applications"
         />
         <div className="relative">
+          {/* Service 1 */}
           <div className="relative z-1 flex items-center h-[39rem] mb-5 p-8 border border-n-1/10 rounded-3xl overflow-hidden lg:p-20 xl:h-[46rem]">
             <div className="absolute top-0 left-0 w-full h-full pointer-events-none md:w-3/5 xl:w-auto">
               <img
@@ -52,6 +53,7 @@ function Services() {
           </div>
 
           <div className="relative z-1 grid gap-5 lg:grid-cols-2">
+            {/* Service 2 */}
             <div className="relative min-h-[39rem] border border-n-1/10 rounded-3xl overflow-hidden">
               <div className="absolute inset-0">
                 <img
@@ -71,7 +73,44 @@ function Services() {
               </div>
               <PhotoChatMessage />
             </div>
+
+            {/* Service 3 */}
+            <div className="p-4 bg-n-7 rounded-3xl overflow-hidden lg:min-h-[46rem]">
+              <div className="py-12 px-4 xl:px-8">
+                <h4 className="h4 mb-4">Video generation</h4>
+                <p className="body-2 mb-[2rem] text-n-3">
+                  The world&apos;s most powerful AI photo and video art
+                  generation engine. What will you create?
+                </p>
+                <ul className="flex items-center justify-between">
+                  {brainwaveServicesIcons.map((item, i) => (
+                    <li
+                      key={i}
+                      className={`flex items-center justify-center rounded-2xl  ${i === 2 ? "w-[3rem] h-[3rem] p-0.25 bg-conic-gradient md:w-[4.5rem] md:h-[4.5rem]" : "flex w-10 h-10 bg-n-6 md:w-15 md:h-15"}`}
+                    >
+                      <div
+                        className={`${i === 2 && "flex items-center justify-center w-full h-full bg-n-7 rounded-[1rem]"}`}
+                      >
+                        <img src={item} width={24} height={24} alt={item} />
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div className="relative h-[20rem] bg-n-8 rounded-xl overflow-hidden md:h-[25rem]">
+                <img
+                  src={service3}
+                  className="w-full h-full object-cover"
+                  width={512}
+                  height={400}
+                  alt="Scary robot"
+                />
+                <VideoChatMessage />
+                <VideoBar />
+              </div>
+            </div>
           </div>
+          <Gradient />
         </div>
       </div>
     </Section>
