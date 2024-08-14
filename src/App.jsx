4b8789af04d9b7ac3467e5c09834bdcd7cf16b8b
vsbron <vsbron@gmail.com>
2024-08-14 13:55:13 +0300

    Started Services section. Added first service. Moved Generating component to UI

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Collaboration from "./components/Collaboration";
 import Header from "./components/Header";
 
 import Hero from "./components/Hero/Hero";
+import Services from "./components/Services";
 
 function App() {
   // Returned JSX
@@ -14,6 +15,7 @@ function App() {
         <Hero />
         <Benefits />
         <Collaboration />
+        <Services />
       </div>
 
       <ButtonGradient />
--- src/ui/Heading.jsx
+++ src/ui/Heading.jsx
@@ -1,8 +1,9 @@
-function Heading({ className, title }) {
+function Heading({ className, title, text }) {
   // Returned JSX
   return (
     <div className={`${className} max-w-[50rem] mx-auto mb-12 lg:mb-20`}>
       {title && <h2 className="h2">{title}</h2>}
+      {text && <p className="body-2 mt-4 text-n-4">{text}</p>}
     </div>
   );
 }
